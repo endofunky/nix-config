b85af940c1f4048e9dbd8101e82930045041041e
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-05 16:07:09 +0100

    Clean up

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -20,25 +20,24 @@ in
     pkgs.vim
   ];
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
 
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  # programs.bash.enable = true;
-  # programs.zsh.enable = true;
-  # programs.fish.enable = true;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.showhidden = true;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
   nix.maxJobs = 1;
   nix.buildCores = 1;
 }
