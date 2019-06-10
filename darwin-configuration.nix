7082d60f445980edc0d23002f38d1f1ab3cf9954
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-10 13:12:33 +0100

    Set pathsToLink

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -9,7 +9,11 @@ in
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = sharedPackages;
-  environment.pathsToLink = ["/share/asdf"];
+  environment.pathsToLink = [
+    "/info"
+    "/etc"
+    "/share"
+  ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
