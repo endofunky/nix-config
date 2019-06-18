97c9d39154a0e968a9c451b0436d61648ee28bc8
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-18 07:48:55 +0100

    Enable acpi_call kernel module

--- machines/xor.nix
+++ machines/xor.nix
@@ -3,9 +3,14 @@
 {
   networking.hostName = "xor";
 
-  boot.initrd.luks.devices = [
-    { name = "root"; device = "/dev/nvme0n1p2"; preLVM = true; allowDiscards = true; }
-  ];
+  boot = {
+    initrd.luks.devices = [
+      { name = "root"; device = "/dev/nvme0n1p2"; preLVM = true; allowDiscards = true; }
+    ];
+
+    kernelModules = [ "acpi_call" ];
+    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+  };
 
   i18n = {
     consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-u22n.psf.gz";
