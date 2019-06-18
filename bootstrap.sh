341acb0281e9e37b713f8c88b57497b293657a7a
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-18 15:17:04 +0100

    Remove darwin support

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/nix" ]; then
-  curl https://nixos.org/nix/install | sh
-  . $HOME/.nix-profile/etc/profile.d/nix.sh
-fi
+echo "Linking NixOS configuration ..."
+sudo ln -sf $PWD/configuration.nix /etc/nixos/configuration.nix
+sudo ln -sf /etc/nixos/hardware-configuration.nix $PWD/hardware-configuration.nix 
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "Bootstrapping nix-darwin ..."
-  nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-  ./result/bin/darwin-installer
-  ln -sf $PWD/darwin-configuration.nix $HOME/.nixpkgs/darwin-configuration.nix
-  . /etc/static/bashrc
-  darwin-rebuild switch
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-  sudo ln -sf $PWD/configuration.nix /etc/nixos/configuration.nix
-  sudo ln -sf /etc/nixos/hardware-configuration.nix $PWD/hardware-configuration.nix
-  sudo chown root.root /etc/nixos/configuration.nix
-  sudo chmod 0644 /etc/nixos/configuration.nix
-fi
-
-echo "Bootstrapping home-manager config ..."
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
-ln -sf $PWD/home.nix $HOME/.config/nixpkgs/home.nix
+# echo "Bootstrapping home-manager config ..."
+# ln -sf $PWD/config.nix $HOME/.config/nixpkgs/config.nix
+# nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+# nix-channel --update
+# nix-shell '<home-manager>' -A install
+# mkdir -p $HOME/.config/nixpkgs
+# ln -sf $PWD/home.nix $HOME/.config/nixpkgs/home.nix
