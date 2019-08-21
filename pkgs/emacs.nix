aa2b469f7fedf312e03006128a110a7f746138ac
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-08-21 16:05:11 +0100

    Update emacs

--- pkgs/emacs.nix
+++ pkgs/emacs.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 
 stdenv.lib.overrideDerivation (pkgs.emacs.override {
   srcRepo = true;
+  withXwidgets = true;
 }) (attrs: rec {
   name = "emacs-${version}${versionModifier}";
   version = "27.0";
@@ -11,10 +12,20 @@ stdenv.lib.overrideDerivation (pkgs.emacs.override {
 
   patches = null;
 
+  configureflags = attrs.configureFlags ++ [
+    "--with-harfbuzz"
+    "--with-json"
+  ];
+
+  buildInputs = attrs.buildInputs ++ [
+    harfbuzz
+    jansson
+  ];
+
   src = fetchFromGitHub {
     owner = "emacs-mirror";
     repo = "emacs";
-    rev = "7aaf500701be3b51c686b7d86c9b505ef5fa9b8f";
-    sha256 = "03l7ffibsxbiyn0xdmc6888ichb2mkrdrl5migrzp478gxy6614w";
+    rev = "0f801d4a5e37e69c4e527830f13f255a6f01360b";
+    sha256 = "1c9c3kanj6ahv371kakz3qilv5pqm2pl0q4bg0d9qbz8amndy8mi";
   };
 })
