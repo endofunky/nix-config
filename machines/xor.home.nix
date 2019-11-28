48cbd3629c5fcd9ad2678f7ce5b5e324bab54bbc
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-11-28 10:55:18 +0000

    Add new MX Master 2S input name

--- machines/xor.home.nix
+++ machines/xor.home.nix
@@ -27,16 +27,5 @@
       name = "Adwaita";
       size = 32;
     };
-
-    profileExtra = ''
-      xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 0.7
-      xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
-      xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
-      xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Disable While Typing Enabled" 1
-      xinput --set-prop "Synaptics TM3289-002" "libinput Accel Speed" 0.7
-      xinput --set-prop "Synaptics TM3289-002" "libinput Tapping Enabled" 1
-      xinput --set-prop "Synaptics TM3289-002" "libinput Natural Scrolling Enabled" 1
-      xinput --set-prop "Synaptics TM3289-002" "libinput Disable While Typing Enabled" 1
-    '';
   };
 }
